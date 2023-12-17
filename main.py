f952f7cbec9fe8280e3e1b8c589e5a324e6da366
mr-mojtaba <mr.mojtaba.mail@gmail.com>
2023-12-18 00:45:17 +0330

    add some commands

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ import jdatetime
 # Need to install ( pip install pdfkit )
 import pdfkit
 
+
+order_number = input("Order Number: ")
+
+context = {
+    "order_number": order_number,
+    "date": jdatetime.date.strftime(jdatetime.date.today(), "%Y/%m/%d")
+
+}
+
 # --------------- Variables ---------------
 
 # Specifying the directory of the template file.
@@ -19,5 +28,17 @@ template = env.get_template('template.html')
 output = template.render(name="mj")
 
 # Creating and writing to the new file.
-with open(r"templates\new-template.html", mode="w", encoding="utf-8") as tm:
+with open(r"templates/template.html", mode="w", encoding="utf-8") as tm:
     tm.write(output)
+
+# Installation path of the wkhtmltopdf.
+wkhtmltopdf = r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"
+
+# Specifying the template file path to convert to HTML.
+file = r"templates/template.html"
+
+# Doing wkhtmltopdf config.
+config = pdfkit.configuration(wkhtmltopdf=wkhtmltopdf)
+
+# Convert html to pdf.
+pdfkit.from_file(file, output_path="inv.pdf", configuration=config)
